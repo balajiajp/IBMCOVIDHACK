d9f59b1d6025d1110e902597638cb5be185ed908
Balajiayyalu <56960216+Balajiayyalu@users.noreply.github.com>
2020-04-23 21:40:52 +0530

    Inital Commit

---
+++ SocialDistanceTracker/SocialDistanceTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialDistanceTracker
+//
+//  Created by Ayyalu  Jeyaprakash, Balaji (Cognizant) on 23/04/20.
+//  Copyright © 2020 Ayyalu  Jeyaprakash, Balaji (Cognizant). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
